a79b8d22ebb918bf2998f0015e443dafe3827ddf
Dhruvin Mehta <dhruvin.mehta25799@gmail.com>
2022-04-11 18:56:38 +0530

    Added routes for cart and wishlist actions.

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ const saltRounds = 10;
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(express.json()); 
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/lovecoffeeDB");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/lovecoffeeDB"
+);
 
 const userSchema = new mongoose.Schema({
-  email: {type: String, unique: true, required: true},
-  password: {type: String, required: true},
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
   cart: Object,
   wishlist: Array,
 });
@@ -125,19 +127,23 @@ app.route("/register").post((req, res) => {
       newUser.save((err) => {
         if (err) {
           res.status(500);
-          if(err.message.split(" ")[0]==="E11000"){
+          if (err.message.split(" ")[0] === "E11000") {
             res.status(403);
-            res.send(JSON.stringify({
-              error: "Email already registered",
-            }))
+            res.send(
+              JSON.stringify({
+                error: "Email already registered",
+              })
+            );
           }
           res.send();
         } else {
           console.log("User register success!");
           res.status(200);
-          res.send(JSON.stringify({
-            message: "Registartion successfull",
-          }));
+          res.send(
+            JSON.stringify({
+              message: "Registartion successfull",
+            })
+          );
         }
       });
     }
@@ -174,16 +180,20 @@ app.route("/login").post((req, res) => {
                   expiresIn: "15m",
                 });
                 res.status(200);
-                res.send(JSON.stringify({
-                  message: "User authenticated.",
-                  token,
-                }))
+                res.send(
+                  JSON.stringify({
+                    message: "User authenticated.",
+                    token,
+                  })
+                );
               } else {
                 console.log("Password invalid");
                 res.status(403);
-                res.send(JSON.stringify({
-                  error: "Invalid Password."
-                }));
+                res.send(
+                  JSON.stringify({
+                    error: "Invalid Password.",
+                  })
+                );
               }
             }
           }
@@ -191,9 +201,11 @@ app.route("/login").post((req, res) => {
       } else {
         console.log("User not found");
         res.status(404);
-        res.send(JSON.stringify({
-          error: "User not found!",
-        }));
+        res.send(
+          JSON.stringify({
+            error: "User not found!",
+          })
+        );
       }
     }
   });
@@ -213,24 +225,466 @@ app.route("/user/cart").get((req, res) => {
       }
     } else {
       if (decoded) {
-        console.log(decoded.foundUser);
         const userId = decoded.foundUser._id;
-        User.findOne({_id: userId}, (err, foundUser) => {
-          if(err){
+        User.findOne({ _id: userId }, (err, foundUser) => {
+          if (err) {
             res.status(500);
             res.send();
           } else {
-            if(foundUser){
+            if (foundUser) {
               res.status(200);
               res.send(JSON.stringify(foundUser.cart));
             }
           }
-        })
+        });
       } else {
         res.status(403);
-        res.send(JSON.stringify({
-          error: "Your token has expired. Please authenticate again."
-        }));
+        res.send(
+          JSON.stringify({
+            error: "Your token has expired. Please authenticate again.",
+          })
+        );
+      }
+    }
+  });
+});
+
+app.route("/user/wishlist").get((req, res) => {
+  const token = req.headers["authorization"];
+  jwt.verify(token, JWT_SECRET_KEY, function (err, decoded) {
+    if (err) {
+      console.log(err);
+      if (err.name === "TokenExpiredError") {
+        res.status(401);
+        res.send();
+      } else {
+        res.status(403);
+        res.send();
+      }
+    } else {
+      if (decoded) {
+        const userId = decoded.foundUser._id;
+        User.findOne({ _id: userId }, (err, foundUser) => {
+          if (err) {
+            res.status(500);
+            res.send();
+          } else {
+            if (foundUser) {
+              res.status(200);
+              res.send(JSON.stringify(foundUser.wishlist));
+            }
+          }
+        });
+      } else {
+        res.status(403);
+        res.send(
+          JSON.stringify({
+            error: "Your token has expired. Please authenticate again.",
+          })
+        );
+      }
+    }
+  });
+});
+
+app
+  .route("/user/wishlist/:productId")
+  .post((req, res) => {
+    const token = req.headers["authorization"];
+    jwt.verify(token, JWT_SECRET_KEY, function (err, decoded) {
+      if (err) {
+        console.log(err);
+        if (err.name === "TokenExpiredError") {
+          res.status(401);
+          res.send();
+        } else {
+          res.status(403);
+          res.send();
+        }
+      } else {
+        if (decoded) {
+          const produtId = req.params.productId;
+          const product = req.body.product;
+          const userId = decoded.foundUser._id;
+          User.findOne({ _id: userId }, (err, foundUser) => {
+            if (err) {
+              res.status(500);
+              res.send();
+            } else {
+              if (foundUser) {
+                foundUser.wishlist.push(product);
+                foundUser.save((err) => {
+                  if (err) {
+                    res.status(500);
+                    res.send();
+                  } else {
+                    res.status(200);
+                    res.send(JSON.stringify(foundUser.wishlist));
+                  }
+                });
+              }
+            }
+          });
+        } else {
+          res.status(403);
+          res.send(
+            JSON.stringify({
+              error: "Your token has expired. Please authenticate again.",
+            })
+          );
+        }
+      }
+    });
+  })
+  .delete((req, res) => {
+    const token = req.headers["authorization"];
+    jwt.verify(token, JWT_SECRET_KEY, function (err, decoded) {
+      if (err) {
+        console.log(err);
+        if (err.name === "TokenExpiredError") {
+          res.status(401);
+          res.send();
+        } else {
+          res.status(403);
+          res.send();
+        }
+      } else {
+        if (decoded) {
+          const produtId = req.params.productId;
+          const product = req.body.product;
+          const userId = decoded.foundUser._id;
+          User.findOne({ _id: userId }, (err, foundUser) => {
+            if (err) {
+              res.status(500);
+              res.send();
+            } else {
+              if (foundUser) {
+                const newList = foundUser.wishlist.filter(
+                  (item) => item._id !== produtId
+                );
+                foundUser.wishlist = newList;
+                foundUser.save((err) => {
+                  if (err) {
+                    res.status(500);
+                    res.send();
+                  } else {
+                    res.status(200);
+                    res.send(JSON.stringify(foundUser.wishlist));
+                  }
+                });
+              }
+            }
+          });
+        } else {
+          res.status(403);
+          res.send(
+            JSON.stringify({
+              error: "Your token has expired. Please authenticate again.",
+            })
+          );
+        }
+      }
+    });
+  });
+
+app.route("/user/cart").get((req, res) => {
+  const token = req.headers["authorization"];
+  jwt.verify(token, JWT_SECRET_KEY, function (err, decoded) {
+    if (err) {
+      console.log(err);
+      if (err.name === "TokenExpiredError") {
+        res.status(401);
+        res.send();
+      } else {
+        res.status(403);
+        res.send();
+      }
+    } else {
+      if (decoded) {
+        const userId = decoded.foundUser._id;
+        User.findOne({ _id: userId }, (err, foundUser) => {
+          if (err) {
+            res.status(500);
+            res.send();
+          } else {
+            if (foundUser) {
+              res.status(200);
+              res.send(JSON.stringify(foundUser.cart));
+            }
+          }
+        });
+      } else {
+        res.status(403);
+        res.send(
+          JSON.stringify({
+            error: "Your token has expired. Please authenticate again.",
+          })
+        );
+      }
+    }
+  });
+});
+
+app
+  .route("/user/cart/:productId")
+  .post((req, res) => {
+    const token = req.headers["authorization"];
+    const type = req.body.type;
+    jwt.verify(token, JWT_SECRET_KEY, function (err, decoded) {
+      if (err) {
+        console.log(err);
+        if (err.name === "TokenExpiredError") {
+          res.status(401);
+          res.send();
+        } else {
+          res.status(403);
+          res.send();
+        }
+      } else {
+        if (decoded) {
+          const productId = req.params.productId;
+          const product = req.body.product;
+          const userId = decoded.foundUser._id;
+          User.findOne({ _id: userId }, (err, foundUser) => {
+            if (err) {
+              res.status(500);
+              res.send();
+            } else {
+              if (foundUser) {
+                if (req.body.action) {
+                  const type = req.body.action;
+                  if (type === "increment") {
+                    foundUser.cart = {
+                      ...foundUser.cart,
+                      cart: foundUser.cart.cart.map((item) =>
+                        item._id === productId
+                          ? { ...item, quantity: +item.quantity + 1 }
+                          : item
+                      ),
+                      totalAmount:
+                        +foundUser.cart.totalAmount +
+                        (+product.price -
+                          +product.price * (+product.discount / 100)),
+                      totalItems: +foundUser.cart.totalItems + 1,
+                    };
+                  }
+                  if (type === "decrement") {
+                    const isSingle = foundUser.cart.cart.find(
+                      (item) => item._id === productId
+                    );
+                    console.log(isSingle);
+                    if (isSingle.quantity === 1) {
+                      foundUser.cart = {
+                        ...foundUser.cart,
+                        cart: foundUser.cart.cart.filter(
+                          (item) => item._id !== productId
+                        ),
+                        totalAmount:
+                          +foundUser.cart.totalAmount -
+                          (+product.price -
+                            +product.price * (+product.discount / 100)),
+                        totalItems: +foundUser.cart.totalItems - 1,
+                        shippingCharges:
+                          foundUser.cart.cart.length === 1
+                            ? foundUser.cart.cart[0].quantity === 1
+                              ? "0"
+                              : "499"
+                            : "499",
+                      };
+                    } else {
+                      foundUser.cart = {
+                        ...foundUser.cart,
+                        cart: foundUser.cart.cart.map((item) =>
+                          item._id === productId
+                            ? { ...item, quantity: item.quantity - 1 }
+                            : item
+                        ),
+                        totalAmount:
+                          +foundUser.cart.totalAmount -
+                          (+product.price -
+                            +product.price * (+product.discount / 100)),
+                        totalItems: +foundUser.cart.totalItems - 1,
+                      };
+                    }
+                  }
+                } else {
+                  foundUser.cart = {
+                    cart: [...foundUser.cart.cart, { ...product, quantity: 1 }],
+                    totalAmount:
+                      +foundUser.cart.totalAmount +
+                      (+product.price -
+                        +product.price * (+product.discount / 100)),
+                    totalItems: +foundUser.cart.totalItems + 1,
+                    shippingCharges: "499",
+                  };
+                }
+                foundUser.save((err) => {
+                  if (err) {
+                    res.status(500);
+                    res.send();
+                  } else {
+                    res.status(200);
+                    res.send(JSON.stringify(foundUser.cart));
+                  }
+                });
+              }
+            }
+          });
+        } else {
+          res.status(403);
+          res.send(
+            JSON.stringify({
+              error: "Your token has expired. Please authenticate again.",
+            })
+          );
+        }
+      }
+    });
+  })
+  .delete((req, res) => {
+    const token = req.headers["authorization"];
+    jwt.verify(token, JWT_SECRET_KEY, function (err, decoded) {
+      if (err) {
+        console.log(err);
+        if (err.name === "TokenExpiredError") {
+          res.status(401);
+          res.send();
+        } else {
+          res.status(403);
+          res.send();
+        }
+      } else {
+        if (decoded) {
+          const produtId = req.params.productId;
+          const product = req.body.product;
+          const userId = decoded.foundUser._id;
+          User.findOne({ _id: userId }, (err, foundUser) => {
+            if (err) {
+              res.status(500);
+              res.send();
+            } else {
+              if (foundUser) {
+                foundUser.cart = {
+                  cart: foundUser.cart.cart.filter(
+                    (item) => item._id !== produtId
+                  ),
+                  totalAmount:
+                    +foundUser.cart.totalAmount -
+                    (+product.price -
+                      +product.price * (+product.discount / 100)) *
+                      +product.quantity,
+                  totalItems: foundUser.cart.totalItems - product.quantity,
+                  shippingCharges:
+                    foundUser.cart.cart.length === 1
+                      ? "0"
+                      : foundUser.cart.shippingCharges,
+                };
+                foundUser.save((err) => {
+                  if (err) {
+                    res.status(500);
+                    res.send();
+                  } else {
+                    res.status(200);
+                    res.send(JSON.stringify(foundUser.cart));
+                  }
+                });
+              }
+            }
+          });
+        } else {
+          res.status(403);
+          res.send(
+            JSON.stringify({
+              error: "Your token has expired. Please authenticate again.",
+            })
+          );
+        }
+      }
+    });
+  });
+
+app.route("/user/cart/move/:productId").post((req, res) => {
+  const token = req.headers["authorization"];
+  const productId = req.params.productId;
+  const product = req.body.product;
+  const action = req.body.action;
+  jwt.verify(token, JWT_SECRET_KEY, function (err, decoded) {
+    if (err) {
+      console.log(err);
+      if (err.name === "TokenExpiredError") {
+        res.status(401);
+        res.send();
+      } else {
+        res.status(403);
+        res.send();
+      }
+    } else {
+      if (decoded) {
+        const userId = decoded.foundUser._id;
+        User.findOne({ _id: userId }, (err, foundUser) => {
+          if (err) {
+            res.status(500);
+            res.send();
+          } else {
+            if (foundUser) {
+              if (action === "WISHLIST_TO_CART") {
+                foundUser.wishlist = foundUser.wishlist.filter(
+                  (item) => item._id !== productId
+                );
+                foundUser.cart = {
+                  ...foundUser.cart,
+                  cart: [...foundUser.cart.cart, { ...product, quantity: 1 }],
+                  totalAmount:
+                    +foundUser.cart.totalAmount +
+                    (+product.price -
+                      +product.price * (+product.discount / 100)),
+                  totalItems: foundUser.cart.totalItems + 1,
+                  shippingCharges:
+                    foundUser.cart.shippingCharges === "0"
+                      ? "499"
+                      : foundUser.cart.shippingCharges,
+                };
+              }
+              if (action === "CART_TO_WISHLIST") {
+                const { quantity: remove, ...rest } = product;
+                foundUser.wishlist = [...foundUser.wishlist, rest];
+                foundUser.cart = {
+                  ...foundUser.cart,
+                  cart: foundUser.cart.cart.filter(
+                    (item) => item._id !== productId
+                  ),
+                  totalAmount:
+                    +foundUser.cart.totalAmount -
+                    (+product.price -
+                      +product.price * (+product.discount / 100)) *
+                      product.quantity,
+                  totalItems: +foundUser.cart.totalItems - product.quantity,
+                  shippingCharges:
+                    foundUser.cart.cart.length === 1
+                      ? "0"
+                      : foundUser.cart.shippingCharges,
+                };
+              }
+
+              foundUser.save((err) => {
+                if (err) {
+                  res.status(500);
+                  res.send();
+                } else {
+                  res.status(200);
+                  res.send(JSON.stringify(foundUser));
+                }
+              });
+            }
+          }
+        });
+      } else {
+        res.status(403);
+        res.send(
+          JSON.stringify({
+            error: "Your token has expired. Please authenticate again.",
+          })
+        );
       }
     }
   });
